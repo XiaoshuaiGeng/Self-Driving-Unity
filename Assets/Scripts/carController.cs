42b03b01e5726bae3af74258492ae4e3a30b3074
XiaoshuaiGeng <genglittlehandsome@hotmail.com>
2021-03-20 08:21:46 +0800

    spawn cars on spawn point(InitiateCar.cs)

---
+++ Assets/Scripts/carController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+    
+    // public GameObject object;
+    public int population;
+    public GameObject car;
+    private GameObject SpawnPoint;
+
+    int i = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPoint = GameObject.Find("SpawnPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(i < population)
+        {
+            Instantiate(car, SpawnPoint.transform.position, Quaternion.identity);
+            i++;
+        }
+
+
+    }
+}
